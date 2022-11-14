3d3585f3461158fc76805f643c1b683306092b2c
wilkyrlx <john_wilkinson@brown.edu>
2022-11-14 10:31:46 -0500

    draggable - error with App

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+import { listData } from './assets/listData'
+import DraggableList from './components/list/DraggableList';
+import Card from './components/card/Card';
+
+export default function App() {
+    return (
+        <>
+            <h1 className="header">
+                React drag and drop list
+            </h1>
+            <DraggableList
+                data={listData}
+                renderItemContent={(item) => LessonCard(item)}
+            />
+        </>
+    );
+}
+
+const LessonCard = item => <Card item={item}/>
+
