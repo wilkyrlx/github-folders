a4d0ee3f67a8e6279ab4c93ed10190f92898de02
wilkyrlx <john_wilkinson@brown.edu>
2022-11-18 16:08:09 -0500

    upkeep

---
+++ frontend/src/ControlPanel.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+function HomeButton() {
+
+}
+
+function AddFolderButton() {
+
+}
+
+function AddManualRepoButton() {
+
+}
+
+function LinkAccountButton() {
+    
+}
