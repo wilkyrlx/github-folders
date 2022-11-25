1d257b3be82cc0d8c46c7a952be007f29fb41d14
wilkyrlx <john_wilkinson@brown.edu>
2022-11-25 18:18:35 -0500

    feat: get all repos from github, display as stripeItem

--- frontend/src/github-interface/GithubReader.tsx
+++ frontend/src/github-interface/GithubReader.tsx
@@ -1,14 +1,14 @@
 import { Octokit } from "octokit"
 import { StripeItemsProps } from "../App"
 import { stripeItem, stripeItemType } from "../components/StripeItem"
-import { Repo } from "../data/Repo"
+import { Repo } from "../data/repo"
 import { githubToken } from "../private/GithubKey"
 
 async function readGithub({setItems, items}: StripeItemsProps) {
     getAllRepos({setItems, items}, githubAPIResponse())
 }
 
-async function githubAPIResponse(): Promise<Repo> {
+async function githubAPIResponse(): Promise<Repo[]> {
     const token = githubToken
     const rawData = await fetch(`https://api.github.com/user/repos`,{
         method: "GET",
@@ -17,35 +17,23 @@ async function githubAPIResponse(): Promise<Repo> {
         }
       });
     const jsonData = await rawData.json()
-    return await new Repo(jsonData[0].name, jsonData[0].full_name, jsonData[0].html_url)
-}
-
-async function mockAPIResponse(): Promise<Repo> {
-    const fileContents: string = `[    {
-        "name": "brown-poker",
-        "full_name": "wilkyrlx/brown-poker",
-        "private": false,
-        "owner": {
-          "login": "wilkyrlx",
-          "type": "User",
-          "site_admin": false
-        },
-        "html_url": "https://github.com/wilkyrlx/brown-poker",
-        "url": "https://api.github.com/repos/wilkyrlx/brown-poker",
-        "visibility": "public",
-      }]`
-
-    return JSON.parse(fileContents)
-}
-
-// TODO: fix types
-async function getAllRepos({setItems, items}: StripeItemsProps, rawData:Promise<Repo>) {
-    // gets current stripeItems
-    const newItems = items.slice();
-    const jsonData: Repo = await rawData;
-
-    newItems.push(new stripeItem({ name: repo.name, link: repo.html_url, typeItem: stripeItemType.REPO , children: [] }));
+    // note: add better error catching here
+    const repoListFull: Repo[] = []
     jsonData.forEach((repo: any) => {
+        repoListFull.push(new Repo(repo.name, repo.full_name, repo.html_url));
+    });
+    return await repoListFull
+}
+
+// async function mockAPIResponse(): Promise<Repo[]> {
+//     return blank
+// }
+
+async function getAllRepos({setItems, items}: StripeItemsProps, repoListPromise:Promise<Repo[]>) {
+    const newItems = items.slice();
+    const repoList: Repo[] = await repoListPromise;
+
+    repoList.forEach((repo: Repo) => {
         newItems.push(new stripeItem({ name: repo.name, link: repo.html_url, typeItem: stripeItemType.REPO , children: [] }));
     });
 
