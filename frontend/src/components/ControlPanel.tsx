c507f552a9c5a4d1c579c4b6169572605cbef49d
wilkyrlx <john_wilkinson@brown.edu>
2022-11-29 20:15:38 -0500

    fix: better UI for manual add repo input bar with fade transition

--- frontend/src/components/ControlPanel.tsx
+++ frontend/src/components/ControlPanel.tsx
@@ -14,10 +14,30 @@ interface ControlledInputProps {
     value: string,
     setValue: Dispatch<SetStateAction<string>>,
     placeholder: string,
-    keyHandler: any
+    keyHandler: any,
+}
+
+interface ExpandingButtonProps {
+    setItems: Dispatch<SetStateAction<stripeItem[]>>,
+    items: stripeItem[],
+    setExpanded: Dispatch<SetStateAction<expandedEnum>>,
+    expanded: expandedEnum
 }
 
 function ControlledInput({ value, setValue, placeholder, keyHandler }: ControlledInputProps) {
+    const inputVar = {
+        hidden: {
+            opacity: 0, transition: {
+                delay: 0.2
+            }
+        },
+        show: {
+            opacity: 1, transition: {
+                delay: 0.2
+            }
+        }
+    };
+
     return (
         <motion.input
             layout
@@ -26,21 +46,24 @@ function ControlledInput({ value, setValue, placeholder, keyHandler }: Controlle
             onChange={(ev) => setValue(ev.target.value)}
             placeholder={placeholder}
             onKeyDown={keyHandler}
+            variants={inputVar}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
         ></motion.input>
     );
 }
 
-function AddFolderButton({setItems, items}: StripeItemsProps) {
-    const [expanded, setExpanded] = useState(false)
+function AddFolderButton({ setItems, items, setExpanded, expanded }: ExpandingButtonProps) {
     const [newFolder, setNewFolder] = useState<string>('');
 
     const handleKeyDown = (event: any) => {
         if (event.key === 'Enter') {
             console.log(newFolder)
             setNewFolder('')
-            setExpanded(false)
+            setExpanded(expandedEnum.NONE)
             const newItems = items.slice();
-            newItems.push(new stripeItem({ name: newFolder, link: "#", typeItem: stripeItemType.DIRECTORY , children: [] }));
+            newItems.push(new stripeItem({ name: newFolder, link: "#", typeItem: stripeItemType.DIRECTORY, children: [] }));
             setItems(newItems);
         }
     }
@@ -49,35 +72,32 @@ function AddFolderButton({setItems, items}: StripeItemsProps) {
     return (
         <motion.div
             layout
-            animate={expanded ? "open" : "closed"}
-            onClick={() => setExpanded(true)}
+            onClick={() => setExpanded(expandedEnum.FOLDER)}
             className="add-folder-button expand-button"
-            data-expanded={expanded}
+            data-expanded={expanded === expandedEnum.FOLDER}
             style={{
                 borderRadius: BUTTON_BORDER_RADIUS
             }}>
 
-            {!expanded && <motion.img layout className="button-img" src="/icons/folder-plus.svg" />}
-            {expanded && <ControlledInput value={newFolder} setValue={setNewFolder} placeholder={"Add New Folder (Beta)"} keyHandler={handleKeyDown} />}
+            {expanded !== expandedEnum.FOLDER && <motion.img layout className="button-img" src="/icons/folder-plus.svg" />}
+            {expanded === expandedEnum.FOLDER && <ControlledInput value={newFolder} setValue={setNewFolder} placeholder={"Add New Folder (Beta)"} keyHandler={handleKeyDown} />}
         </motion.div>
     )
 
 }
 
-function AddManualRepoButton({setItems, items}: StripeItemsProps) {
-    // TODO: set other types to boolean
-    const [expanded, setExpanded] = useState<boolean>(false)
-    const [newRepo, setNewRepo] = useState<string>('');
 
+function AddManualRepoButton({ setItems, items, setExpanded, expanded }: ExpandingButtonProps) {
+    const [newRepo, setNewRepo] = useState<string>('');
 
     // TODO: fix issue where user can submit a repo name that already exists. Maybe random directory? random/dummy
     const handleKeyDown = (event: any) => {
         if (event.key === 'Enter') {
             console.log(newRepo)
             setNewRepo('')
-            setExpanded(false)
+            setExpanded(expandedEnum.NONE)
             const newItems = items.slice();
-            newItems.push(new stripeItem({ name: newRepo, link: "#", typeItem: stripeItemType.REPO , children: [] }));
+            newItems.push(new stripeItem({ name: newRepo, link: "#", typeItem: stripeItemType.REPO, children: [] }));
             setItems(newItems);
         }
     }
@@ -85,25 +105,24 @@ function AddManualRepoButton({setItems, items}: StripeItemsProps) {
     return (
         <motion.div
             layout
-            onClick={() => setExpanded(!expanded)}
+            onClick={() => setExpanded(expandedEnum.MANUAL)}
             className="expand-button"
-            data-expanded={expanded}
+            data-expanded={expanded === expandedEnum.MANUAL}
             style={{
                 borderRadius: BUTTON_BORDER_RADIUS
             }}>
 
-            {!expanded && <motion.img layout className="button-img" src="/icons/search.svg" /> }
-            {expanded && <ControlledInput value={newRepo} setValue={setNewRepo} placeholder={"Paste Repository Link"} keyHandler={handleKeyDown} />}
-
+            {expanded !== expandedEnum.MANUAL && <motion.img layout className="button-img" src="/icons/search.svg" />}
+            {expanded === expandedEnum.MANUAL && <ControlledInput value={newRepo} setValue={setNewRepo} placeholder={"Paste Repository Link"} keyHandler={handleKeyDown} />}
         </motion.div>
     )
 }
 
-function SettingsButton({setItems, items}: StripeItemsProps) {
+function SettingsButton({ setItems, items }: StripeItemsProps) {
     return (
         <motion.div
             layout
-            onClick={() => readGithub({setItems, items})}
+            onClick={() => readGithub({ setItems, items })}
             className="expand-button"
             style={{
                 borderRadius: BUTTON_BORDER_RADIUS
@@ -113,7 +132,8 @@ function SettingsButton({setItems, items}: StripeItemsProps) {
     )
 }
 
-function BackButton({setItems, items}: StripeItemsProps) {
+// FIXME: not working right now
+function BackButton({ setItems, items }: StripeItemsProps) {
     return (
         <motion.div
             layout
@@ -128,14 +148,27 @@ function BackButton({setItems, items}: StripeItemsProps) {
     )
 }
 
-function ControlPanel({setItems, items}: StripeItemsProps) {
+class expandedEnum {
+    static readonly FOLDER = new expandedEnum()
+    static readonly MANUAL = new expandedEnum()
+    static readonly NONE = new expandedEnum()
+
+    // private to disallow creating other instances of this type
+    private constructor() {
+    }
+}
+
+function ControlPanel({ setItems, items }: StripeItemsProps) {
+
+    const [expanded, setExpanded] = useState<expandedEnum>(expandedEnum.NONE)
+
     return (
         <div className="control-panel">
             <LayoutGroup>
-                <BackButton setItems={setItems} items={items}/>
-                <AddFolderButton setItems={setItems} items={items}/>
-                <AddManualRepoButton setItems={setItems} items={items}/>
-                <SettingsButton setItems={setItems} items={items}/>
+                <BackButton setItems={setItems} items={items} />
+                <AddFolderButton setItems={setItems} items={items} setExpanded={setExpanded} expanded={expanded} />
+                <AddManualRepoButton setItems={setItems} items={items} setExpanded={setExpanded} expanded={expanded} />
+                <SettingsButton setItems={setItems} items={items} />
             </LayoutGroup>
         </div>
     )
--- frontend/src/data/StripeItem.ts
+++ frontend/src/data/StripeItem.ts
@@ -1,10 +1,10 @@
 /**
-	 * This class functions as an enum with fields:
-	 * path - path to the image icon
-	 * target - repo opens in new tab, directory executes on page
-	 * TODO: check how chrome extensions work, repo may not have to open in new tab
-	 */ 
- class stripeItemType {
+* This class functions as an enum with fields:
+* path - path to the image icon
+* target - repo opens in new tab, directory executes on page
+* TODO: check how chrome extensions work, repo may not have to open in new tab
+*/
+class stripeItemType {
     static readonly REPO = new stripeItemType("/icons/command-line.svg", "_blank")
     static readonly DIRECTORY = new stripeItemType("/icons/folder.svg", "_self")
 
@@ -16,18 +16,18 @@
 interface stripeItemProps {
     name: string;
     link: string;
-    typeItem:stripeItemType;
-    children: stripeItem[] 
+    typeItem: stripeItemType;
+    children: stripeItem[]
 }
 
 // TODO: need to determine if doubly linked list is necessary or not
 class stripeItem {
     public name: string;    // name (visible to user)
     public link: string;    // link to github repo (or # for directory)
-    public typeItem:stripeItemType; //REPO or DIRECTORY
+    public typeItem: stripeItemType; //REPO or DIRECTORY
     public id: number;  // ID, critical for framer-motion list order
     public children: stripeItem[];  // list of stripeItems in directory (directory only, NOT for repo)
-    constructor(item:stripeItemProps){
+    constructor(item: stripeItemProps) {
         this.name = item.name;
         this.link = item.link;
         this.id = cyrb53(item.name);
@@ -37,18 +37,18 @@ class stripeItem {
 }
 
 // hashing function, credits to bryc. Used for generating a unique numeric ID (critical for framer-motion list)
-const cyrb53 = (str:string, seed = 0) => {
+const cyrb53 = (str: string, seed = 0) => {
     let h1 = 0xdeadbeef ^ seed,
-      h2 = 0x41c6ce57 ^ seed;
+        h2 = 0x41c6ce57 ^ seed;
     for (let i = 0, ch; i < str.length; i++) {
-      ch = str.charCodeAt(i);
-      h1 = Math.imul(h1 ^ ch, 2654435761);
-      h2 = Math.imul(h2 ^ ch, 1597334677);
+        ch = str.charCodeAt(i);
+        h1 = Math.imul(h1 ^ ch, 2654435761);
+        h2 = Math.imul(h2 ^ ch, 1597334677);
     }
-    
+
     h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^ Math.imul(h2 ^ (h2 >>> 13), 3266489909);
     h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^ Math.imul(h1 ^ (h1 >>> 13), 3266489909);
-    
+
     return 4294967296 * (2097151 & h2) + (h1 >>> 0);
 };
 
